76bee75bf79331570d46452f0aef678735156ccb
NnicanBuak <mybailengdb@gmail.com>
2023-02-17 10:33:33 +0800

    try config tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/node_modules/@nuxtjs/tailwindcss/dist/runtime/tailwind.css',
+    configPath: 'tailwind.config.cjs',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   typescript: {
-    shim: false
+    shim: false,
   }
 })
